18a561d503e999f9d0da057c26354231a79cd4f7
Roan Rothrock <roan.rothrock@hotmail.com>
2023-11-01 22:19:32 -0500

    Added notes and stuff

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ extern "C"
 {
 #include "deflate.h"
 #include "inflate.h"
-#include<options.h>
+#include "options.h"
 }
 
 using namespace std;
@@ -25,6 +25,28 @@ int main(int argc, char **argv)
   // argv[1] = executable
   // argv[2] = .deb file
 
-
+  // steps
+  // 
+  // if we're on Debian:
+  // 	dpkg does all of this for us
+  // 	to extract everything:
+  // 	`mkdir tmp`
+  // 	`dpkg-deb -R <.deb> tmp`
+  // 	to recompile:
+  // 	`dpkg-deb -b tmp`
+  // 	and remember:
+  // 	`rm -r tmp`
+  // else:
+  // 	to extract:
+  // 	`ar -x <.deb>`
+  // 	`mkdir control data`
+  // 	if extension is `.gz`:
+  // 		`gzip -d *.gz`
+  // 	if extension is `.xz`:
+  // 		`xz -d *.gz`
+  // 	`tar -xf control.tar -C ./control/`
+  // 	`tar -xf data.tar -C ./data/`
+  // 	to recompile:
+  // 	
 }
 
